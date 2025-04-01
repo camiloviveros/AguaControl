9382a2c66e5ad2d4261279f86fe173819ed4b2ba
camiloviveros <camiloviveros111@gmail.com>
2025-04-01 08:43:28 -0500

    cambios

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,26 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-import nextPlugin from "@next/eslint-plugin-next";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-  recommendedConfig: {},
+  recommendedConfig: { extends: ["eslint:recommended"] }
 });
 
-const eslintConfig = [
-  {
-    plugins: {
-      next: nextPlugin,
-    },
-  },
-  ...compat.extends("next/core-web-vitals"),
+export default [
   {
     languageOptions: {
       ecmaVersion: 2022,
       sourceType: "module",
-    },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
+    }
   },
+  ...compat.extends("next/core-web-vitals")
 ];
-
-export default eslintConfig;
